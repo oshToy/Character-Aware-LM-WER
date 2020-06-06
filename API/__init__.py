e358d2b3bf644c5479d64f3bc25d56a53edacae3
Oshri Mahlev <oshri1992@gmail.com>
2020-06-06 17:03:17 +0300

    refactor

--- API/__init__.py
+++ API/__init__.py
@@ -40,25 +40,69 @@ class CharacterAwareLmWer:
         self.session, self.m, self.fasttext_model, self.max_word_length, self.char_vocab, self.word_vocab = init_model()
         self.rnn_state = self.session.run(self.m.initial_rnn_state)
 
-    def sentence_wer(self, sentence, acoustics):
-        # rnn outputs
+    def sentence_wer(self, sentence: str, acoustics: []):
+        """
+        returns wer of given sentence
+        :param sentence:(str) single sentence
+        :param acoustics:(array of 4-floats arrays) acoustics data of each word - 4 numbers describe:
+        1.first feature provide by Kaldi
+        2.sec feature provide by Kaldi
+        3. feature 1 + feature 2
+        4. feature 1 - feature 2
+
+        :return:
+
+         Examples:
+            model = CharacterAwareLM()
+            model.sentence_wer(sentence='hello world', acoustics=[[0.1, 0.2, 0.3, 0.4], [0.5, 0.6, 0.7, 0.8]])
+            returns:
+                <class 'list'>: [[array([[0.16053563]], dtype=float32)]]
+
+
+        """
         return get_sentence_wer(sentence, acoustics, self.session, self.m, self.fasttext_model, self.max_word_length,
                                 self.char_vocab,
                                 self.rnn_state)
 
-    def rescore_kaldi_n_best(self, n_best_sentence, n_best_acoustics):
-        # rnn outputs
-        return get_rescore_kaldi_n_best(n_best_sentence, n_best_acoustics, self.session, self.m, self.fasttext_model, self.max_word_length,
+    def rescore_kaldi_n_best(self, n_best_sentence: [], n_best_acoustics: []):
+        """
+        rescore Kaldi N best guesses
+        :param n_best_sentence:(sorted array of Kaldi n best, first is first Kaldi guess) Kaldi n best guesses
+        :param n_best_acoustics:(array of 4-floats arrays) acoustics data of each word - 4 numbers describe:
+        1.first feature provide by Kaldi
+        2.sec feature provide by Kaldi
+        3. feature 1 + feature 2
+        4. feature 1 - feature 2
+
+        :return:(sorteddict of str: array) each key is sentence and each value is wer array of floats with shape (1)
+
+        Examples:
+            model = CharacterAwareLM()
+            model.rescore_kaldi_n_best(n_best_sentence=['hello world', 'hello', 'wow hiz'],
+                               n_best_acoustics=[
+                                   [[0.1, 0.2, 0.3, 0.4], [0.5, 0.6, 0.7, 0.8]],
+                                   [[0.1, 0.2, 0.3, 0.5]],
+                                   [[0.1, 0.2, 0.3, 0.6], [0.1, 0.6, 0.7, 0.8]]
+                               ])
+            returns:
+                KeysView(sorteddict({
+                    'hello': [[array([[0.1604901]], dtype=float32)]],
+                    'hello world': [[array([[0.16053563]], dtype=float32)]],
+                    'wow hiz': [[array([[0.16049011]], dtype=float32)]]
+                    }))
+        """
+        return get_rescore_kaldi_n_best(n_best_sentence, n_best_acoustics, self.session, self.m, self.fasttext_model,
+                                        self.max_word_length,
                                         self.char_vocab,
                                         self.rnn_state)
 
 
 if __name__ == '__main__':
     model = CharacterAwareLmWer()
-    print(model.sentence_wer(sentence='hello world', acoustics=[[0.1, 0.2, 0.3, 0.4], [0.5, 0.6, 0.7, 0.8]]))
-    print(model.rescore_kaldi_n_best(n_best_sentence=['hello world', 'hello', 'wow hiz'],
-                                     n_best_acoustics=[
-                                         [[0.1, 0.2, 0.3, 0.4], [0.5, 0.6, 0.7, 0.8]],
-                                         [[0.1, 0.2, 0.3, 0.5]],
-                                         [[0.1, 0.2, 0.3, 0.6], [0.1, 0.6, 0.7, 0.8]]
-                                     ]))
+    model.sentence_wer(sentence='hello world', acoustics=[[0.1, 0.2, 0.3, 0.4], [0.5, 0.6, 0.7, 0.8]])
+    model.rescore_kaldi_n_best(n_best_sentence=['hello world', 'hello', 'wow hiz'],
+                               n_best_acoustics=[
+                                   [[0.1, 0.2, 0.3, 0.4], [0.5, 0.6, 0.7, 0.8]],
+                                   [[0.1, 0.2, 0.3, 0.5]],
+                                   [[0.1, 0.2, 0.3, 0.6], [0.1, 0.6, 0.7, 0.8]]
+                               ])
--- API/rescore_kaldi_n_best.py
+++ API/rescore_kaldi_n_best.py
@@ -2,17 +2,6 @@ from API.sentence_wer import get_sentence_wer
 from blist import sorteddict
 
 def get_rescore_kaldi_n_best(kaldi_n_best_sentence, kaldi_n_best_acoustics, session, m, fasttext_model, max_word_length, char_vocab, rnn_state):
-    """
-
-    :param kaldi_n_best: list of sentences
-    :param session:
-    :param m:
-    :param fasttext_model:
-    :param max_word_length:
-    :param char_vocab:
-    :param rnn_state:
-    :return: sorted dict key: sent value: wer. the keys order are in rescore order.
-    """
     rescore_kaldi_n_best = {}
 
     for sentence, acoustics in zip(kaldi_n_best_sentence, kaldi_n_best_acoustics):
